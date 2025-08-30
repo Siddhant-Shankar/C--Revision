2a505da0750d51e3e71dbabc84091012b5d013f2
Siddhant Shankar <siddhantshankar02@gmail.com>
2025-08-30 12:18:51 -0500

    Classes done

---
+++ Classes/classes_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+
+using namespace std; 
+
+/* 
+There are default operators that every return type has. we can overload them if we want to
+
+*/
+
+
+class CVector {
+  public:
+    int x,y;
+    CVector () {};
+    CVector (int a,int b) : x(a), y(b) {}
+    CVector operator + (const CVector&);
+};
+
+CVector CVector::operator+ (const CVector& param) {
+  CVector temp;
+  temp.x = x + param.x;
+  temp.y = y + param.y;
+  return temp;
+}
+
+int main() { 
+
+}
